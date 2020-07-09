edd1359c16303c76acf416ec243824db50212b98
ninaszymor <33628387+ninaszymor@users.noreply.github.com>
2020-07-09 11:39:47 +0100

    New translations team-chooser-finished-team-chooser.py (Marathi)

--- mr-IN/solutions/team-chooser-finished-team-chooser.py
+++ mr-IN/solutions/team-chooser-finished-team-chooser.py
@@ -2,48 +2,48 @@
 
 from random import choice
 
-#create a list of players from a file
+#फाइलमधून खेळाडूंची यादी तयार करा
 players = []
 file = open('players.txt', 'r')
 players = file.read().splitlines()
 print('Players:', players)
 
-#create a list of team names from a file
+#फाईलमधून टीमच्या नावांची यादी तयार करा
 teamNames = []
 file = open('teamNames.txt', 'r')
 teamNames = file.read().splitlines()
 print('Team names:', teamNames)
 
-#create empty team lists
+#टीमच्या रिकाम्या याद्या तयार करा
 teamA = []
 teamB = []
 
-#loop until there are no players left
+#जोपर्यंत कोणतेही खेळाडू शिल्लक नाहीत तोपर्यंत लूप करा
 while len(players) > 0:
   
-  #choose a random player for team A
+  #team A साठी कोणताही खेळाडू निवडा
   playerA = choice(players)
   teamA.append(playerA)
-  #remove the player from the players list
+  #players यादीतून खेळाडू काढून टाका
   players.remove(playerA)
   
-  #break out of the loop if there are no players left
+  #कोणतेही खेळाडू शिल्लक नसल्यास लूपमधून बाहेर या
   if players == []: 
     break
   
-  #choose a random player for team B
+  #team B साठी कोणताही खेळाडू निवडा
   playerB = choice(players)
   teamB.append(playerB)
-  #remove the player from the players list
+  #players यादीतून खेळाडू काढून टाका
   players.remove(playerB)
 
-#choose random team names for the 2 teams
+#2 टीम्स साठी कोणते ही नावे निवडा
 teamNameA = choice(teamNames)
 teamNames.remove(teamNameA)
 teamNameB = choice(teamNames)
 teamNames.remove(teamNameB)
 
-#print the teams
+#टीम्स प्रिंट करा
 print('\nHere are your teams:\n')
 print(teamNameA, teamA)
 print(teamNameB, teamB)
